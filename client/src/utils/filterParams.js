ad3b1b93299f42d20c9e2fdf7dc5c303694c35c7
showboat051 <rashad.davis87@yahoo.com>
2018-07-04 20:49:40 -0500

    erased everything for scratch

--- client/src/utils/filterParams.js
+++
@@ -1,8 +0,0 @@
-// Takes in a params object and returns the object with null/empty keys removed
-export default params =>
-  Object.keys(params)
-    .filter(key => params[key])
-    .reduce((acc, curr) => {
-      acc[curr] = params[curr];
-      return acc;
-    }, {});
