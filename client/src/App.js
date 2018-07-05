f6a73000548517fea439eccf27b9c6c54f707b53
Gian Clemons <gian.d.clemons@gmail.com>
2018-07-05 08:41:30 -0500

    added profile and create-event routes for pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Saved from "./pages/Saved";
+import Profile from "./pages/Profile";
+import Events from "./pages/Events";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
@@ -12,6 +14,8 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/saved" component={Saved} />
+        <Route exact path="/profile" component={Profile}/>
+        <Route exact path="/create-event" component={Events}/>
         <Route component={NoMatch} />
       </Switch>
     </div>
