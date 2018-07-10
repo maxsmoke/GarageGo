594bd6d060da3400ab589af69de3f920f850de8d
showboat051 <rashad.davis87@yahoo.com>
2018-07-09 19:29:58 -0500

    functional file, ready to build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { Jumbotron, Button } from 'reactstrap';
 // import Jumbotron from "./Jumbotron";
+import Home from './pages/Home';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Jumbotron>
-        <h1 className="display-3">Welcome to GarageGO!</h1>
-        <p className="lead">This is where you will find any and all future garage sales in your area.</p>
-        <p>Click down below to learn more</p>
-        <p className="lead">
-          <Button color="primary">Learn More</Button>
-        </p>
-        <hr className="my-2" />
-
-      </Jumbotron>
+     <Home>
         
+        </Home>
       </div>
     );
   }
